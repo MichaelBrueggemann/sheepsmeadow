84328a9dc4eb6650d843e4f7fea0caa861b71372
m_brue <m.brueggemann7@web.de>
2024-03-06 17:14:15 +0100

    add attributes to model

--- src/Model.java
+++ src/Model.java
@@ -1,11 +1,21 @@
 import sim.engine.*;
+import sim.field.grid.ObjectGrid2D;
 
 public class Model extends SimState 
 {
+    // create spatial representation for the model (a "field"). This is where all agents "live"
+    private ObjectGrid2D meadow = new ObjectGrid2D(10, 10);
+
+    // set maximum number of individuals contained in this model
+    private int number_of_individuals = 5;
+
+
     public Model(long seed)
     {
     super(seed);
     }
+
+
 
     public static void main(String[] args)
     {
