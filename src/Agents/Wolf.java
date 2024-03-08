9b6c85a81a185abb3bd6f24430282f6dbe1156bd
m_brue <m.brueggemann7@web.de>
2024-03-08 17:33:52 +0100

    add basic agent

--- src/Agents/Wolf.java
+++ src/Agents/Wolf.java
@@ -2,4 +2,16 @@ package Agents;
 
 public class Wolf {
     
+    private int id;
+
+    public Wolf(int id)
+    {
+        this.id = id;
+    }
+
+    
+    public int getId()
+    {
+        return this.id;
+    }
 }
