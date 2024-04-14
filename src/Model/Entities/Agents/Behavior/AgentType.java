fce0c163fbd74d163c82496ab941f7e199713514
m_brue <m.brueggemann7@web.de>
2024-04-14 16:21:37 +0200

    enum for AgentType

---
+++ src/Model/Entities/Agents/Behavior/AgentType.java
@@ -0,0 +1,10 @@
+package Model.Entities.Agents.Behavior;
+
+/**
+ * This defines types fo each Agent (equivalent to their class). Used to decide which "Action" should be performed from an "ActionList".
+ */
+public enum AgentType {
+    SHEEP,
+    WOLF,
+    GRASS
+}
