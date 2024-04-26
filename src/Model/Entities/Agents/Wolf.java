65c3f8a7ba44129282813c9291d98351433bf8d5
m_brue <m.brueggemann7@web.de>
2024-04-27 00:43:22 +0200

    adjust to new implementation of a "ruleSet"

--- src/Model/Entities/Agents/Wolf.java
+++ src/Model/Entities/Agents/Wolf.java
@@ -1,8 +1,15 @@
 package Model.Entities.Agents;
 
 import java.awt.Color;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.PriorityQueue;
 
+import Model.Entities.Agents.Behavior.Actions.GeneralActionComparator;
+import Model.Entities.Agents.Behavior.Actions.DefaultMove;
+import Model.Entities.Agents.Behavior.Actions.GeneralAction;
 import Model.Entities.Objects.Grass;
+
 import sim.engine.SimState;
 import sim.field.grid.ObjectGrid2D;
 
@@ -13,8 +20,36 @@ public class Wolf extends Agent
 
     public Wolf(int id, int energy, ObjectGrid2D grid)
     {
+
+        // construct all attributes of an "Agent"
         super(id, Color.gray, energy, grid);
-        this.addPriorityClass(0,Sheep.class); // priority 0
+
+        // Comparator needed to decide the order of two "GeneralAction"s
+        Comparator<GeneralAction> generalActionComparator = new GeneralActionComparator();
+
+        // create "Action"s for this agents "ActionList"
+        GeneralAction defaultMove = new DefaultMove(0);
+        // ..
+
+        this.ruleSet = new PriorityQueue<>(generalActionComparator);
+        ruleSet.add(defaultMove);
+
+        // // store all PQs in a HashMap
+        // HashMap<AgentType, PriorityQueue<GeneralAction>> wolfActionMap = new HashMap<>();
+
+        // // add all Sheep-based "Action"s
+        // wolfActionMap.put(AgentType.SHEEP, sheepBasedActions);
+
+        // // add all Wolf-based "Action"s
+        // wolfActionMap.put(AgentType.WOLF, wolfBasedActions);
+
+        // // add all Grass-based "Action"s
+        // wolfActionMap.put(AgentType.GRASS, grassBasedActions);
+
+        
+
+
+        this.addPriorityClass(0, Sheep.class); // priority 0
         this.addPriorityClass(1, Wolf.class); // priority 1
         this.addPriorityClass(2, Grass.class); // priority 2
     }
