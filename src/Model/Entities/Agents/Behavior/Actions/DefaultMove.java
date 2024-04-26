1417e40fdb2c94f4450b8812afd6217269beb5f0
m_brue <m.brueggemann7@web.de>
2024-04-27 00:43:59 +0200

    add "DefaultMove" Action

---
+++ src/Model/Entities/Agents/Behavior/Actions/DefaultMove.java
@@ -0,0 +1,64 @@
+package Model.Entities.Agents.Behavior.Actions;
+
+import Model.Entities.Agents.Agent;
+import Model.Entities.Objects.Grass;
+import Model.Neighbourhood.Cell;
+
+
+public class DefaultMove extends GeneralAction
+{
+
+    public DefaultMove(int priority)
+    {
+        super(
+            "DefaultMove",
+            "Move the Agent to a free neighbouring cell and reduce it's energy by 1", 
+            priority
+        );
+    }
+
+
+    @Override
+    /**
+     * Applies this GeneralAction for a given Agent and a neighbouring cell.
+     * @param agent Agent, who perform this GeneralAction
+     * @param cell neighbouring cell, on which the this GeneralAction should be executed
+     */
+    public void execute(Agent agent, Cell cell)
+    {
+        // reduce energy of agent by 1
+        agent.setEnergy(agent.getEnergy() - 1);
+
+        try 
+        {
+            // move agent to new cell
+            agent.updateGridPosition(cell.getLocation().getX(), cell.getLocation().getY());
+        } 
+        catch (Exception e) 
+        {
+            // GridPositionOccupiedException can't occur, as the condition checks that the cell has to be grass (therefore is free)
+        }
+        
+    }
+
+
+    @Override
+    public boolean checkCondition(Cell cell) 
+    {
+        if (cell.getEntity() instanceof Grass)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+    }
+
+
+    
+}
+
+
+    
