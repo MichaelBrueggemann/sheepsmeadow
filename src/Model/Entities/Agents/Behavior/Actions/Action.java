748a790baef444f674afe6bee41f0bb6cbd36f47
m_brue <m.brueggemann7@web.de>
2024-04-27 13:04:34 +0200

    change definition of Interface

--- src/Model/Entities/Agents/Behavior/Actions/Action.java
+++ src/Model/Entities/Agents/Behavior/Actions/Action.java
@@ -1,21 +1,21 @@
 package Model.Entities.Agents.Behavior.Actions;
 
 import Model.Entities.Agents.Agent;
-import Model.Neighbourhood.Cell;
+import Model.Neighbourhood.Neighbourhood;
 
 public interface Action {
     
     /**
      * This method encapsulates the behavior, that should be performed by the action.
      * @param agent Agent object, which perform the action
-     * @param cell Object containing information about the cell
+     * @param neighbourhood Object containing information about the neighbourhood
      */
-    public void execute(Agent agent, Cell cell);
+    public void execute(Agent agent, Neighbourhood neighbourhood);
 
     /**
-     * A condition, that has to be fullfilled in order to execute the "Action".
-     * @param cell Object containing information about the cell
+     * A condition, that has to be fullfilled in order to execute the "Action". Checks if the condition can be applied to at least one cell of "neighbourhood".
+     * @param neighbourhood Object containing information about the neighbourhood
      * @return true if condition is fullfilled, else false
      */
-    public boolean checkCondition(Cell cell);
+    public boolean checkCondition(Neighbourhood neighbourhood);
 }
