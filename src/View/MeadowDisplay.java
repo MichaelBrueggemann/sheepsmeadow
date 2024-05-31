4c0785a51d459f37be0fb9dffa46b14b74683017
m_brue <m.brueggemann7@web.de>
2024-05-31 16:08:40 +0200

    change size of numbers in the meadow display

--- src/View/MeadowDisplay.java
+++ src/View/MeadowDisplay.java
@@ -55,6 +55,9 @@ public class MeadowDisplay
                                 // draw the label
                                 String label = Integer.toString(agent.getEnergy()); // Or whatever logic you use to get the label
 
+                                Font font = new Font("Calibri", 1, 18);
+                                graphics.setFont(font);
+
                                 // get width of the string and height of the font to correct the position of the label
                                 FontMetrics fm = graphics.getFontMetrics();
                                 int textWidth = fm.stringWidth(label);
